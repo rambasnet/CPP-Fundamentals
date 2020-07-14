3874b2e5610052e165db1969f41a3f594b05a632
Ram Basnet <rambasnet@gmail.com>
2020-07-14 16:27:19 -0600

    added Ch08 and Ch09

---
+++ demo_programs/Ch03/carrots/carrots.cpp
@@ -0,0 +1,21 @@
+/*
+Kattis problem - https://open.kattis.com/problems/carrots
+Solving for Carrots
+
+Algorithm steps:
+- read only the first line (two numbers)
+- output the second number
+*/
+
+#include <iostream>
+#include <cstdio> // printf
+
+using namespace std;
+
+int main() {
+    int N, P;
+    cin >> N >> P;
+    //cout << P << endl;
+    printf("%d\n", P);
+    return 0;
+}
