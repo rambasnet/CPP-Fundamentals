2fd3c6bb5488d55eb817d290f7929bcd0c0337da
Ram Basnet <rambasnet@gmail.com>
2020-08-15 16:59:06 -0600

    updated conditionals.cpp lab

--- labs/conditionals.cpp
+++ labs/conditionals.cpp
@@ -1,10 +1,12 @@
 /*
-User-define Functions, Testing, and Loop Lab
+Conditional Lab
 
 FIXME1: Write programmer information
 FIXME2: Today's Date
 
-A menu-driven program to play with user-defined functions, automated testing and loop
+Program finds statistical values of two given numbers using user-defined functions.
+
+It's menu-driven program that demonstrates user-defined functions, automated testing conditional statements and loops
 */
 
 #include <iostream>
@@ -46,6 +48,8 @@ double findSmaller(const double &, const double &);
  
 void test();
 
+bool program();
+
 // function clears the screen system call
 // NOTE: system call is not a security best pracice!
 void clearScreen() {
@@ -55,55 +59,6 @@ void clearScreen() {
     #else
         system("clear");
     #endif
-}
-
-// the crux of the program that uses all the defined functions
-// the program is menu driven
-bool program() {
-    int option = 1; // variable to store user entered option
-    double num1=0, num2=0; // variables to store two numbers entered by user
-    // display menu options
-    printMenu();
-    // Input validation
-    do {
-        if (cin >> option && option >= 1 && option <= 8) {
-            //input is valid, break loop
-            break;
-        }
-        else {
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << "Invalid option, please enter a value between 1 and 8" << endl;
-        }
-    } while (true);
-            
-    // Call the function(s) or perform some operations based on user input
-    switch(option) {
-        case 1:
-        {
-            getTwoNumbers(num1, num2);
-            // call findSum function; store the returned value in sum variable
-            double sum = findSum(num1, num2);
-            printf("%.2f + %.2f = %.2f\n", num1, num2, sum);
-            break;
-        }
-        case 2:
-        {
-            //FIXME: call getTwoNumbers function
-            //FIXME: call findDifference function and print the returned value
-            break;
-        }
-        case 3:
-        {
-            //FIXME: call getTwoNumbers function
-            //FIXME: call findProduct function and print the returned value
-            break;
-        }
-        // FIXME: finish the rest of the cases 4...7
-        default: // must be option 8
-            return false; // exit the program
-    }
-    return true;
 }
 
 int main(int argc, char* argv[]) {
@@ -153,34 +108,35 @@ double findSum(const double &n1, const double &n2) {
 }
 
 double findDifference(const double &n1, const double &n2) {
-    //FIXME - subtract n2 from n1 and return the difference
+    //FIXME3 - subtract n2 from n1 and return the difference
     return 0;
 }
 
 double findProduct(const double &n1, const double &n2) {
-    //FIXME - multiply n1 by n2 and return the product
+    //FIXME4 - multiply n1 by n2 and return the product
     return 0;
 }
 
 void findAverage(const double &n1, const double &n2, double &avg) {
-    //FIXME - find the average of n1 and n2 and update avg
-    //FIXME - Must call findSum function to find the sum of n1 and n2
+    //FIXME5 - find the average of n1 and n2 and update avg
+    //FIXME6 - Must call findSum function to find the sum of n1 and n2
     // Note: this void function doesn't return a value but
     // the average will be stored in avg
 }   
 
 double findLarger(const double &n1, const double &n2) {
-    //FIXME - find the larger of n1 and n2 and return it
-    return 0;
+    //find the larger of n1 and n2 and return it
+    double larger = (n1 >= n2) ? n1 : n2;
+    return larger;
 }
 
 double findSmaller(const double &n1, const double &n2) {
-    //FIXME - find the smaller of n1 and n2 and return it
+    //FIXME7 - find the smaller of n1 and n2 and return it
     return 0;
 }
 
 double findQuotient(const double &n1, const double &n2) {
-    //FIXME - divide n1 by n2 and return the quotient
+    //FIXME8 - divide n1 by n2 and return the quotient
     return 0;
 }
 
@@ -191,8 +147,68 @@ void test() {
     assert(result == expected); // test case 1
     assert(findSum(-5, 10.5) == 5.5); // test case 2
 
-    // FIXME – Using assert function write at least 2 test cases for each of the following functions
+    // FIXME9 – Using assert function write at least 2 test cases for each of the following functions
     // findDifference(), findProduct(), findLarger(),
     // findSmaller(), findQuotient(), findAverage()
     printf("%s\n", "all test cases passed...");
+}
+
+// the crux of the program that uses all the defined functions
+// the program is menu driven
+bool program() {
+    int option = 1; // variable to store user entered option
+    double num1=0, num2=0; // variables to store two numbers entered by user
+    // display menu options
+    printMenu();
+    // Input validation
+    do {
+        if (cin >> option && option >= 1 && option <= 8) {
+            //input is valid, break loop
+            break;
+        }
+        else {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid option, please enter a value between 1 and 8" << endl;
+        }
+    } while (true);
+            
+    // Call the function(s) or perform some operations based on user input
+    switch(option) {
+        case 1:
+        {
+            // get two numbers and store them into num1 and num2 using function
+            getTwoNumbers(num1, num2);
+            // call findSum function; store the returned value in sum variable
+            double sum = findSum(num1, num2);
+            // display the result with proper description
+            printf("%.2f + %.2f = %.2f\n", num1, num2, sum);
+            break;
+        }
+        case 2:
+        {
+            //FIXME10: call getTwoNumbers function
+            //FIXME11: call findDifference function and print the result
+            break;
+        }
+        case 3:
+        {
+            //FIXME12: get two numbers and find their product using functions
+            break;
+        }
+        // FIXME14: complete the rest of the cases 4, 6, and 7
+        case 5:
+        {
+            // get two numbers
+            getTwoNumbers(num1, num2);
+            // find the larger of the two numbers
+            double max = findLarger(num1, num2);
+            // print the result
+            printf("larger between %.2f & %.2f is %.2f\n", num1, num2, max);
+            break;
+        }
+        default: // must be option 8
+            return false; // exit the program
+    }
+    return true;
 }
