9fc39cc22918e155664ee3a51eb0bad8af8d2eeb
Ram Basnet <rambasnet@gmail.com>
2020-08-15 19:49:40 -0600

    added pointers lab

---
+++ labs/loops.cpp
@@ -0,0 +1,98 @@
+/*
+    Loops Lab
+    Updated By: FIXME1
+    CSCI 111
+    Date: FIXME2
+
+    Program prints geometric shapes of given height with * using loops
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+void printTriangle(int height) {
+    //Function takes height as an argument to print the triangle
+    //of that height with *
+    int row = 1;
+    // row
+    while (row <= height) {
+        // column
+        for(int col = 1; col<=row; col++)
+            cout << "* ";
+        row += 1;
+        cout << endl;
+    }
+}
+
+
+void printFlippedTriangle(int height) {
+    /* 
+    Implement the function that takes height as an argument
+    and prints a triangle with * of given height.
+    Triangle of height 5, e.g., should look like the following.
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+    
+    */
+    // FIXME3 ...
+}
+
+
+/*  
+FIXME4
+Design and implement a function that takes an integer as height and
+prints square of the given height with *.
+Square of height 5, e.g., would look like the following.
+
+*  *  *  *  *  
+*  *  *  *  *   
+*  *  *  *  *   
+*  *  *  *  *   
+*  *  *  *  *   
+
+*/
+
+// function clears the screen system call
+// NOTE: system call is not a security best pracice!
+void clearScreen() {
+    // use "cls" in windows and "clear" command in Mac and Linux
+    #ifdef _WIN32
+        system("clS");
+    #else
+        system("clear");
+    #endif
+}
+
+int main(int argc, char* argv[]) {
+    // FIXME5 add a loop to make the program to continue to run until the user wants to quit
+    // FIXME6 call clearScreen function to clear the screen for each round of the loop
+    int height;
+    cout << "Program prints geometric shapes of given height with *\n";
+    cout << "Please enter the height of the shape: ";
+    cin >> height;
+    // call printTriangle function passing user entered height
+    printTriangle(height);
+
+    // FIXME7
+    // Call printFlippedTriangle passing proper argument
+    // Manually test the function
+
+    // FIXME6
+    // Call the function defined in FIXME4 passing proper argument
+    // Manually test the function
+
+    // FIXME9
+    // prompt user to enter y/Y to continue anything else to quit
+
+    // FIXME10
+    // Use conditional statements to break the loop or continue the loop
+
+    return 0;
+}
---
+++ labs/pointers.cpp
@@ -0,0 +1,120 @@
+//  Pointers, enum type and namespace lab
+//  Update by: FIXME1
+//  Date: FIXME2
+//  Program demonstrates the use of preprocessor directive, namespace, enum type,
+//  and pointers.
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+// macro function
+#ifdef _WIN32
+#define clear() system("cls")
+#else
+#define clear() system("clear")
+#endif
+
+// user-defined enumerated type OPERATION
+enum OPERATION { ADD, MULTIPLY, SUBTRACT, QUIT };
+
+// C++ way of defining type alias
+using big_int = long long int;
+
+// C way of defining type alias
+typedef long long int large_int;
+// both large_int and big_int are alias to long long int type
+
+// declare MyFunctions namespace
+namespace MyFunctions {
+    // function prototypes defined withing the namespace
+    big_int findSum(const big_int *, const big_int *);
+    big_int findLarger(const big_int *, const big_int *);
+    large_int findProduct(const large_int &, const large_int &);
+    large_int findDifference(const large_int &, const large_int &);
+}
+
+void showMenu(void);
+OPERATION getOperation(char);
+
+int main(int argc, char* argv[]) {
+    char input;
+    big_int *num1; // a pointer to point that points to unsigned int aka big_int data type
+    num1 = new big_int; //allocate memory in heap and store the address in num1
+    big_int *num2 = new big_int; //declare num2 as a dynamic variable
+    large_int *sum = new large_int; //declare sum as a dynamic variable
+
+    // FIXME3 - declare dynamic variables to store product and difference values
+    // Must use these variables to store the returned values from functions
+
+    OPERATION oper;
+    
+    //FIXME4: Add do... while loop to continue the program until the user wants to quit
+    //FIXME5: call clear function defined above to clear the screen
+    showMenu();
+    cin >> input;
+    oper = getOperation(input);
+    switch (oper) {
+        case ADD:
+            cout << "Enter two whole numbers separated by space: ";
+            // store the data into pointers
+            cin >> *num1 >> *num2;
+            // passing pointers to findSum function
+            *sum = MyFunctions::findSum(num1, num2);
+            printf("%lld + %lld = %lld\n", *num1, *num2, *sum);
+            break;
+            // FIXME6: complete the rest of the cases to perform other operations
+        default:
+            break;
+    }
+    
+    cin.ignore(1000, '\n');
+    cout << "Good bye! Enter to exit the program...";
+    cin.get();
+    return 0;
+}
+
+void showMenu(void) {
+    cout << "Enter one of the following menu options:\n";
+    cout << "[1] Add two big_ints\n";
+    cout << "[2] Multiply two big_ints\n";
+    cout << "[3] Subtract one big_int from another\n";
+    cout << "[4] Quit the program\n";
+    cout << "Enter your choice [1-4]: ";
+}
+
+// function returns OPERATION type given character choice
+OPERATION getOperation(char choice) {
+    OPERATION op;
+    switch(choice)
+    {
+        case '1':
+            op = ADD;
+            break;
+        case '2':
+            op = MULTIPLY;
+            break;
+        case '3':
+            op = SUBTRACT;
+            break;
+        default:
+            op = QUIT;
+    }
+    return op;
+}
+
+// must use MyFunctions namespace to resolve findSum function
+big_int MyFunctions::findSum(const big_int *n1, const big_int *n2) {
+    // deference pointers n1 and n2 before adding their values
+    return (*n1) + (*n2);
+}
+
+// FIXME7: define findLarger function declared inside MyFunctions namespace
+// function returns the larger of the two given values
+large_int MyFunctions::findProduct(const large_int &n1, const large_int &n2) {
+    return n1 * n1;
+}
+
+//FIXME8: define findDifference function declared inside MyFunctions namespace
+// return the value of second big_int subtracted from the first
