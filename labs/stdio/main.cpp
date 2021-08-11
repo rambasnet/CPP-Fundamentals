da036cb1d152fe35a75521e13b1f429b45b5e31f
Ram Basnet <rambasnet@gmail.com>
2021-08-11 15:39:23 -0600

    update stdio lab

--- labs/stdio/main.cpp
+++ labs/stdio/main.cpp
@@ -1,6 +1,6 @@
 /*
-    ASCII Lab
-    ASCII Art - Using variables
+    StdIO Lab
+    ASCII Art - using literals and variables
     
     Updated By: <Your name> #FIXME1
     Date: ... #FIXME2
@@ -9,11 +9,11 @@
 
     Algorithm steps: 
     1. Use variables to store data/values
-    2. Write a series of cout statements to print the values.
+    2. Write a series of cout statements to print the data/values
 */
 
 #include <iostream> //library for input and output
-#include <string>
+#include <string> // library for string data
  
 using namespace std; //resolve cout, cin, and endl names
  
@@ -31,10 +31,11 @@ int main()
     string line1 = "  |\\_/|   **********************    (\\_/)\n";
     cout << line1;
 
-    //FIXME6: use variable to print the second 
-    //FIXME7: print the the third line
+    //FIXME6: use variable to print the second line line of the graphic
+    //FIXME7: print the third line of the graphics
     //FIXME8: use variable to print the fourth line
-    //FIXME9: use variable to print the fift line
+    //FIXME9: use variable to print the fifth line
+    //Note: You can add more lines or print more ASCII arts of your choice if you'd like...
 
     cout << "\nGood bye... hit enter to exit the program: " << '\n';
     // FIXME10: make the console wait for user input
