ae5232a4c06c985bcf125208bda4a39f49708963
Ram Basnet <rambasnet@gmail.com>
2021-09-14 10:28:55 -0600

    update stdio/demo1

--- demos/stdio/demo1/main.cpp
+++ demos/stdio/demo1/main.cpp
@@ -1,7 +1,7 @@
 // demonstrates standard input and output
 // specifically >> and getline
 // compile and run the program as it is
-// TODO: fix it by uncommenting cin >> line, so the getline works propertly!
+// TODO: fix it by uncommenting cin >> ws; line, so the getline works propertly!
 
 #include <iostream>
 #include <string>
@@ -16,13 +16,15 @@ int main() {
     // parse the input stream
     // >> reads and discards leading whitespace until printable character is found
     cin >> n1 >> ch >> n2; // leaves behind \n
-    // ws makes the input wait until non-whitespace character is found
-    // need to use it carefully!
     cout << n1 << " + " << n2 << " = " << n1+n2 << endl;
     cout << "Hi, what's your name?: ";
-    //cin >> ws; // discard any whitespace character and stop at the character
+    // cin >> ws; // discard any whitespace character and stop at the character
+    // ws makes the input wait until non-whitespace character is found
+    // need to use it carefully!
     getline(cin, name);
     cout << "Nice meeting you, " << name << endl;
-    cout << "Good bye...\n";
+    cout << "Good bye!\n";
+    cout << "Press enter to quit the program...";
+    cin.get();
     return 0;
 }
