07793497b6c302d60060d4003a9ed27cf7e2fcb9
Ram Basnet <rambasnet@gmail.com>
2022-09-29 21:31:02 -0600

    update two stones lab

--- labs/conditionals/twostones/main.cpp
+++ labs/conditionals/twostones/main.cpp
@@ -8,10 +8,10 @@ Date: FIXME
 Read and solve the Kattis problem: https://open.kattis.com/problems/twostones 
 
 Algorithm Steps:
-		1. Read the number of stones
-		2. Check if the number of stones is odd or even
-    3.a. If the number is odd, Alice wins.
-    3.b. Otherwise, Bob wins.
+  1. Read the number of stones
+	2. Check if the number of stones is odd or even
+  3.a. If the number is odd, Alice wins.
+  3.b. Otherwise, Bob wins.
 */
 
 #include <iostream>
@@ -27,18 +27,17 @@ void testOddOrEven();
 // given a positive integer, function returns the answer as expected by Kattis
 string answer(const int unsigned);
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   if (argc == 2 and string(argv[1]) == string("test")) {
     // FIXME1: call testOddOrEven function that unit tests oddOrEven function
     // FIXME2: call the test function that unit tests answer function
   }
   else {
-    unsigned stones=0;
+    unsigned int stones=0;
     // FIXME3: readin the stones
     cout << answer(stones) << endl;
   }
-	return 0;
+  return 0;
 }
 
 string oddOrEven(const int unsigned number) {
