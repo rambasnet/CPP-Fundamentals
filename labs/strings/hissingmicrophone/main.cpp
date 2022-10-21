62edfc7411dc287a4e4bf6e676a46330d4dbf1bb
Ram Basnet <rambasnet@gmail.com>
2022-10-21 09:40:06 -0600

    updaet hiss lab

--- labs/strings/hissingmicrophone/main.cpp
+++ labs/strings/hissingmicrophone/main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
 }
 
 string answer(const string &line) {
-    // FIXME3 - implement algorithm in step 2; return "hiss" or "no hiss"
+    // FIXME3 - implement algorithm step 2; return "hiss" or "no hiss"
     return "hiss";
 }
 
