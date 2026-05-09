a7053335c5b92ee88380ffab6e148ce084e67d82
Ram Basnet <rambasnet@gmail.com>
2026-05-09 21:40:35 +0000

    update Textual User Interface - TUI

--- notebooks/demos/tui/hello/src/main.cpp
+++ notebooks/demos/tui/hello/src/main.cpp
@@ -1,16 +1,20 @@
 /*
 TUI with NCurses demo.
 */
-#include <ncurses.h>
+#include <locale.h>
+// #include <ncurses.h>
+// We will use ncursesw for wide character support (Unicode)
+#include <ncursesw/ncurses.h>
 #include "include/util.hpp"
 
 int main()
 {
-    initscr();             // Start ncurses mode
-    move(5, 10);           // Move cursor to row 5, column 10
-    printw("Hello World"); // Print to memory
-    refresh();             // Show it on the real screen
-    getch();               // Wait for user input
+    setlocale(LC_ALL, "");                // This enables UTF-8
+    initscr();                            // Start ncurses mode
+    move(5, 10);                          // Move cursor to row 5, column 10
+    printw("Hello World 🌍 テスト 测试"); // Print to memory; supporting Unicode characters
+    refresh();                            // Show it on the real screen
+    getch();                              // Wait for user input
     float ans = Demo::add(3.5f, 4.2f);
     move(7, 10);                                                 // Move cursor to row 7, column 10
     printw("3.5 + 4.2 = %.2f", ans);                             // Print the result of addition
